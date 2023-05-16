1b26108476d4148d3907430b7f3a53cc837bcc53
MaiGia96 <130622456+MaiGia96@users.noreply.github.com>
2023-05-16 22:33:58 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng sinh nhật';
+const titleCard = 'Tặng bé Trinh';
+const contentCard = 'Chúc mừng ngày em ra đời. Sau bao năm thì đứa trẻ ngày ấy đã trở thành một cô gái đáng yêu, chúc em một ngày thật nhiều niềm vui và ăn bánh kem no nê.💘';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
